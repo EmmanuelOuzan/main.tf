38b5c5070e32ec627f6f14521da16cf9ccabf6e5
Emmanuel Ouzan <Emmanuel.ouzan@gmail.com>
2023-05-11 12:08:58 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,36 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.75.0"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
   region = "us-west-2"
 }
 
-variable "bucketname" {
-  type    = string
-  default = "env0-acme-bucket"
-}
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
 
-resource "random_string" "random" {
-  length  = 5
-  special = false
-  lower   = true
-  upper   = false
-  number  = true
-}
-
-module "s3" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.0.0"
-
-  bucket = "${var.bucketname}-${random_string.random.id}"
-  acl    = "private"
-}
-  
-output "s3_bucket_arn" {
-  value = module.s3.s3_bucket_arn
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
